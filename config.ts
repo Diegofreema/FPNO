e0dcba7b8d6b47a640ccb6852565695b69c43b96
diego <diegofreeman78@gmail.com>
2025-05-08 14:52:50 +0100

    build: add Android and iOS project configuration files
    This commit introduces the necessary configuration files for building the Android and iOS projects. It includes Gradle build scripts, AndroidManifest.xml, Info.plist, and other essential files for both platforms. The changes enable the project to be built and run on Android and iOS devices, ensuring compatibility with the Expo and React Native ecosystems.
    
    The commit also includes the addition of resource files such as icons, splash screens, and color definitions, which are required for the app's UI. These changes are part of the initial setup to prepare the project for development and deployment on both platforms.

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const API_ENDPOINT = process.env.EXPO_PUBLIC_API_END_POINT!;
+export const PROJECT_ID = process.env.EXPO_PUBLIC_PROJECT_ID!;
+
+export const PROJECT_NAME = process.env.EXPO_PUBLIC_PROJECT_NAME!;
