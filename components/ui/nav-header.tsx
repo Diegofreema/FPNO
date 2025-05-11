0c70ba8f2c4a83b9feaca1d1bc3acfa8bbe53f6f
diego <diegofreeman78@gmail.com>
2025-05-11 19:49:37 +0100

    feat(ui): add NavHeader component and integrate into Detail screen
    Introduce a reusable NavHeader component to standardize navigation headers across the app. Integrate it into the Detail screen to enhance UI consistency and improve user experience. The NavHeader supports custom content, styling, and navigation actions.

---
+++ components/ui/nav-header.tsx
@@ -0,0 +1,63 @@
+import { StyleProp, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+
+import { SubTitle } from '@/components/typography/subtitle';
+import { AntDesign } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import React from 'react';
+
+type NavHeaderProps = {
+  title: string;
+  style?: StyleProp<ViewStyle>;
+  leftContent?: () => React.JSX.Element;
+  avatarContent?: React.JSX.Element;
+  color?: string;
+  onPress?: () => void;
+};
+export const NavHeader = ({
+  title,
+  leftContent: LeftContent,
+  style,
+  avatarContent: AvatarContent,
+  color = 'black',
+  onPress: handlePress,
+}: NavHeaderProps) => {
+  const onPress = (): void => {
+    handlePress && handlePress();
+    router.back();
+  };
+  return (
+    <View
+      style={[
+        {
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          zIndex: 4,
+          alignItems: 'center',
+        },
+        style,
+      ]}
+    >
+      <TouchableOpacity
+        onPress={onPress}
+        style={{ padding: 5, flexDirection: 'row', alignItems: 'center' }}
+      >
+        <AntDesign name="arrowleft" color={color} size={30} />
+        {AvatarContent && AvatarContent}
+      </TouchableOpacity>
+      <SubTitle
+        text={title}
+        textStyle={{
+          fontFamily: 'NunitoBold',
+          fontSize: RFPercentage(2.5),
+          color: color,
+        }}
+      />
+      {LeftContent ? (
+        <LeftContent />
+      ) : (
+        <View style={{ width: 50, height: 50 }} />
+      )}
+    </View>
+  );
+};
