adcd0abba94b527eb3edd709690018b00e3fd8ec
diego <diegofreeman78@gmail.com>
2025-05-12 07:10:54 +0100

    refactor: update appwrite configuration and remove unused imports
    Update the appwrite client configuration to use a hardcoded platform name instead of a dynamic one. Remove the unused `PROJECT_NAME` import and comment out the debug log. Also, add overrides in `package.json` to ensure compatibility with specific versions of `expo-file-system` and `react-native`.

--- db/appwrite.ts
+++ db/appwrite.ts
@@ -1,14 +1,14 @@
-import { API_ENDPOINT, PROJECT_ID, PROJECT_NAME } from '@/config';
+import { API_ENDPOINT, PROJECT_ID } from '@/config';
 import { Client, Databases, Models } from 'react-native-appwrite';
 
-console.log({ API_ENDPOINT, PROJECT_ID, PROJECT_NAME });
+// console.log({ API_ENDPOINT, PROJECT_ID, PROJECT_NAME });
 let client: Client;
 let databases: Databases;
 client = new Client();
 client
   .setEndpoint(API_ENDPOINT)
   .setProject(PROJECT_ID) // Your Project ID
-  .setPlatform(PROJECT_NAME);
+  .setPlatform('com.diegodark.FPNO');
 
 databases = new Databases(client);
 export { client, databases, Models };
