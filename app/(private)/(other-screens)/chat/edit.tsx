25aa48fa2c9bbcc7cbf374c63d8d97d61c474123
diego <diegofreeman78@gmail.com>
2025-05-15 18:51:08 +0100

    feat(chat): add edit screen and update room info screen
    Introduce a new edit screen for chat rooms and enhance the room info screen to include a navigation option to the edit screen. This improves the user experience by allowing room details to be modified directly from the room info screen.

---
+++ app/(private)/(other-screens)/chat/edit.tsx
@@ -0,0 +1,13 @@
+import { NavHeader } from '@/components/ui/nav-header';
+import { Wrapper } from '@/components/ui/wrapper';
+import React from 'react';
+
+const EditScreen = () => {
+  return (
+    <Wrapper>
+      <NavHeader title="Edit room" />
+    </Wrapper>
+  );
+};
+
+export default EditScreen;
--- app/(private)/(other-screens)/chat/room-info.tsx
+++ app/(private)/(other-screens)/chat/room-info.tsx
@@ -1,15 +1,24 @@
-import { useLocalSearchParams } from 'expo-router';
+import { NavHeader } from '@/components/ui/nav-header';
+import { Wrapper } from '@/components/ui/wrapper';
+import { ChatMenu } from '@/features/chat/components/chat-menu';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
 
 const RoomInfo = () => {
   const { roomId } = useLocalSearchParams<{ roomId: string }>();
-  console.log({ roomId });
-
+  const router = useRouter();
+  const onSelect = () => {
+    router.push(`/chat/edit?${roomId}`);
+  };
   return (
-    <View>
-      <Text>RoomInfo</Text>
-    </View>
+    <Wrapper>
+      <NavHeader
+        title=""
+        leftContent={() => (
+          <ChatMenu menuItems={[{ onSelect: onSelect, text: 'Edit' }]} />
+        )}
+      />
+    </Wrapper>
   );
 };
 
